88700489078ee1ac0d6ff5667d833e463dabf62e
jaeyuun <ghghwodbs@gmail.com>
2024-02-07 10:04:41 +0900

    [Fix] Camera Image Save

--- PushPop/Assets/3.Script/03_Jaeyun/CameraManager.cs
+++ PushPop/Assets/3.Script/03_Jaeyun/CameraManager.cs
@@ -7,8 +7,8 @@ using UnityEngine.UI;
 
 public class CameraManager : MonoBehaviour
 {
-	public Image captureImage;
-	public Texture2D captureTexture; // camera capture texture
+	[SerializeField] private Image captureImage;
+	private Texture2D captureTexture; // Create Image
 
 	public void CameraOpen() // button method
     {
@@ -20,7 +20,7 @@ public class CameraManager : MonoBehaviour
     {
 		string _filePath = Application.persistentDataPath + "/Profile";
 		if (!File.Exists(_filePath))
-		{
+		{ // 해당 Directory 없을 시 생성
 			Directory.CreateDirectory(_filePath);
 		}
 
@@ -29,7 +29,7 @@ public class CameraManager : MonoBehaviour
 			Debug.Log("Image path: " + _filePath);
 			if (path != null)
 			{
-				// Create a Texture2D from the captured image
+				// Create Image
 				Texture2D texture = NativeCamera.LoadImageAtPath(path, 2048);
 				if (texture == null)
 				{
@@ -37,28 +37,34 @@ public class CameraManager : MonoBehaviour
 					return;
 				}
 
-				// camera load
+				// Camera load
 				GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
 				quad.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 2.5f;
 				quad.transform.forward = Camera.main.transform.forward;
 				quad.transform.localScale = new Vector3(1f, texture.height / (float)texture.width, 1f);
 
-				Material material = quad.GetComponent<Renderer>().material;
+                #region Material
+                Material material = quad.GetComponent<Renderer>().material;
 				if (!material.shader.isSupported) material.shader = Shader.Find("Legacy Shaders/Diffuse");
-
 				material.mainTexture = texture;
-				captureTexture = texture;
+				#endregion
 
+				// capture texture 표시
+				captureTexture = texture;
 				Rect rect = new Rect(0, 0, captureTexture.width, captureTexture.height);
 				captureImage.sprite = Sprite.Create(captureTexture, rect, new Vector2(0.5f, 0.5f));
 
-				Texture2D readableTexture = GetReadableTexture(texture);
-				Texture2D snap = new Texture2D(readableTexture.width, readableTexture.height);
+				// capture texture save
+				Texture2D readableTexture = GetReadableTexture(texture); // current Texture
+				
+				/*Texture2D snap = new Texture2D(readableTexture.width, readableTexture.height); // copy Texture
 				snap.SetPixels(readableTexture.GetPixels());
-				snap.Apply();
+				snap.Apply();*/
 
+				byte[] texturePNGByte = readableTexture.EncodeToPNG(); // texture to pngByte encode
+				string fileName = $"{_filePath}/00_00.png";
 				// File.WriteAllBytes($"{GameManager.instance.UID}_{GameManager.instance.Profile_Index}.png", snap.EncodeToPNG());
-				File.WriteAllBytes($"{_filePath}/00_00.png", snap.EncodeToPNG());
+				File.WriteAllBytes(fileName, texturePNGByte); // file save
 
 				Destroy(quad, 5f);
 			}
@@ -67,7 +73,7 @@ public class CameraManager : MonoBehaviour
 
 	private Texture2D GetReadableTexture(Texture2D source)
     {
-		RenderTexture renderTexture = RenderTexture.GetTemporary(
+        RenderTexture renderTexture = RenderTexture.GetTemporary(
 				source.width,
 				source.height,
 				0,
@@ -75,15 +81,13 @@ public class CameraManager : MonoBehaviour
 				RenderTextureReadWrite.Linear
 			);
 
-		Graphics.Blit(source, renderTexture);
-		RenderTexture previous = RenderTexture.active;
+		Graphics.Blit(source, renderTexture); // Texture에 맞게 Render Texture 복사
 		RenderTexture.active = renderTexture;
-		Texture2D readableTexture = new Texture2D(source.width, source.height);
 
+		Texture2D readableTexture = new Texture2D(source.width, source.height, TextureFormat.RGB24, false);
 		readableTexture.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
 		readableTexture.Apply();
-		RenderTexture.active = previous;
-		RenderTexture.ReleaseTemporary(renderTexture);
+
 		return readableTexture;
     }
 }
