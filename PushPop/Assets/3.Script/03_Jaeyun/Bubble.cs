f60b74eb7b6727c2e0d6cada081d58ed7189a64c
Jaeyun Lee <ghghwodbs@gmail.com>
2024-02-11 19:53:35 +0900

    [Feat] Bubble

--- PushPop/Assets/3.Script/03_Jaeyun/Bubble.cs
+++ PushPop/Assets/3.Script/03_Jaeyun/Bubble.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -9,37 +10,70 @@ public enum Mode
     Bomb,
 }
 
-public class PushBubble : IBubble
-{
-    public virtual void BubbleTouch(GameObject bubble)
-    {
-        bubble = EventSystem.current.currentSelectedGameObject; // select한 object
-
-    }
-
-    public virtual void BubbleMovement()
-    {
-
-    }
-}
-
-public class SpeedBubble : IBubble
-{
-
-}
-
-public class MemoryBubble : IBubble
-{
-
-}
-
-public class BombBubble : IBubble
-{
-
-}
-
-public class Bubble : MonoBehaviour
+public class Bubble : MonoBehaviour, IPointerDownHandler, IBubble
 { // bubble prefab마다 달아줄 script
-    protected Mode mode; // game mode
+    protected Mode gameMode = Mode.PushPush; // game mode
+    private bool isMoving = false;
 
+    public void OnPointerDown(PointerEventData eventData)
+    { // 현재 오브젝트 내부에서 클릭하는 순간 1회 호출
+
+        Vector2 bubblePosition = transform.position;
+        Vector2 touchPosition = eventData.position;
+
+        switch (gameMode)
+        {
+            case Mode.PushPush:
+                PushPushMode(bubblePosition, touchPosition);
+                break;
+            case Mode.Speed:
+                break;
+            case Mode.Memory:
+                break;
+            case Mode.Bomb:
+                break;
+        }
+    }
+
+    public void PushPushMode(Vector2 _bubblePosition, Vector2 _touchPosition)
+    {
+        // Bubble Move
+        Vector2 dir = _bubblePosition - _touchPosition; // Touch Position의 반대 방향
+
+        StopAllCoroutines();
+        StartCoroutine(BubbleMove_Co(dir));
+    }
+
+
+    public void SpeedMode(Vector2 _bubblePosition, Vector2 _touchPosition)
+    {
+        int touchCount = Random.Range(1, 11); // 1 ~ 10회 터치 가능
+
+        // Bubble Move
+        Vector2 dir = _bubblePosition - _touchPosition;
+        dir.y = 0f; // 좌우로만 이동
+
+        StopAllCoroutines();
+        StartCoroutine(BubbleMove_Co(dir));
+        touchCount--;
+    }
+
+    public void BombMode()
+    {
+    }
+
+    public void MemoryMode()
+    {
+    }
+
+    private IEnumerator BubbleMove_Co(Vector2 _dir)
+    {
+        float timer = 2f;
+        while (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            transform.Translate(_dir * (Time.deltaTime * 0.5f));
+            yield return null;
+        }
+    }
 }
--- PushPop/Assets/3.Script/03_Jaeyun/IBubble.cs
+++ PushPop/Assets/3.Script/03_Jaeyun/IBubble.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public interface IBubble
 {
-    void BubbleTouch() { }
-    void BubbleMovement() { }
+    void PushPushMode(Vector2 _bubblePosition, Vector2 _touchPosition);
+    void SpeedMode(Vector2 _bubblePosition, Vector2 _touchPosition);
+    void MemoryMode();
+    void BombMode();
 }
