2220b5f6ae54cbd1c55c723c1686c7c41d29ce3c
minki0324 <147981201+minki0324@users.noreply.github.com>
2024-02-12 22:24:53 +0900

    [Fix] Network Scene UI
    1. Network 기능 확인
    2. UI Asset 적용

---
+++ PushPop/Assets/3.Script/00_Result/Network/SelectCharacter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectCharacter : MonoBehaviour
+{
+
+    #region Unity Callback
+    #endregion
+
+    #region Other Method
+    // Network Room에서 사용할 캐릭터 선택 Btn 연동 Method
+    public void ChoiceCharacter(int index)
+    {
+        // index에 따라서 플레이어 캐릭터 다른걸로 나오도록 수정
+
+    }
+    #endregion
+}
