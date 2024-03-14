baac5ad70b22df51c8160c9b6990aae9c78986cc
jaeyuun <ghghwodbs@gmail.com>
2024-03-14 17:52:49 +0900

    [Fix] Jenkins setting

--- PushPop/Assets/Editor/Jenkins.cs
+++ PushPop/Assets/Editor/Jenkins.cs
@@ -9,12 +9,17 @@ public class Jenkins
     {
         BuildPlayerOptions options = new BuildPlayerOptions();
         // 씬 추가
-        options.scenes = new[] { "Assets/00_Maingame.unity", "Assets/01_Network.unity" };
+        List<string> scenes = new List<string>();
+        foreach (var scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            scenes.Add(scene.path);
+        }
+        options.scenes = scenes.ToArray();
         // 타겟 경로(빌드 결과물이 여기 생성됨)
-        options.locationPathName = $"../Build/PushPop.apk";
+        options.locationPathName = "../Build/PushPopBulidTest.apk";
         // 빌드 타겟
         options.target = BuildTarget.Android;
-
         // 빌드
         BuildPipeline.BuildPlayer(options);
     }
