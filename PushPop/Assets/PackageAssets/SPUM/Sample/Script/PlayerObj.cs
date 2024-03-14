41c346b3e3650c9cde1d1b06c3f21649a40d2a41
Minjun <129065548+Nangdi@users.noreply.github.com>
2024-03-14 11:51:37 +0900

    [Fix] Sync , NetworkScene organize

--- PushPop/Assets/PackageAssets/SPUM/Sample/Script/PlayerObj.cs
+++ PushPop/Assets/PackageAssets/SPUM/Sample/Script/PlayerObj.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Mirror;
 using UnityEngine;
 using UnityEngine.Events;
-using Mirror;
 
 [System.Serializable]
 public class MyEvent : UnityEvent<PlayerObj.PlayerState>
@@ -13,7 +11,12 @@ public class MyEvent : UnityEvent<PlayerObj.PlayerState>
 [ExecuteInEditMode]
 public class PlayerObj : NetworkBehaviour
 {
+    private PlayerManager playerManager;
+    private NetworkIdentity myIdentity;
     public SPUM_Prefabs _prefabs;
+    public NetworkAnimator netAni;
+    public PlayerEmotionControl emotionControl;
+    [SyncVar] public int avatarIndex;
     public float _charMS;
     public enum PlayerState
     {
@@ -23,14 +26,16 @@ public class PlayerObj : NetworkBehaviour
         death,
     }
     private PlayerState _currentState;
-    public PlayerState CurrentState{
+    public PlayerState CurrentState
+    {
         get => _currentState;
-        set {
+        set
+        {
             _stateChanged.Invoke(value);
             _currentState = value;
         }
     }
-   
+
     private MyEvent _stateChanged = new MyEvent();
 
     public Vector3 _goalPos;
@@ -42,9 +47,12 @@ public class PlayerObj : NetworkBehaviour
         if (isLocalPlayer)
         {
             transform.position = Vector3.zero;
-            PlayerManager playerManager = FindObjectOfType<PlayerManager>();
-            playerManager.ConnectPrefabs = GetComponent<NetworkIdentity>();
+            playerManager = FindObjectOfType<PlayerManager>();
+            myIdentity = GetComponent<NetworkIdentity>();
+            Debug.Log("내 아이덴티티 : " + myIdentity);
+            playerManager.ConnectPrefabs = myIdentity;
             playerManager._nowObj = this;
+
             SendProfile();
         }
 
@@ -53,28 +61,28 @@ public class PlayerObj : NetworkBehaviour
 
 
     }
-    private void PlayStateAnimation(PlayerState state){
+    private void PlayStateAnimation(PlayerState state)
+    {
         _prefabs.PlayAnimation(state.ToString());
     }
     void Update()
     {
-        transform.position = new Vector3(transform.position.x,transform.position.y,transform.localPosition.y * 0.01f);
-        switch(_currentState)
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.localPosition.y * 0.01f);
+        switch (_currentState)
         {
             case PlayerState.idle:
-            break;
+                break;
 
             case PlayerState.run:
-            DoMove();
-            break;
+                DoMove();
+                break;
         }
-
-
     }
 
     private void OnDestroy()
     {
         RemoveProfile();
+        RemoveServerList();
     }
 
     private void OnApplicationQuit()
@@ -84,33 +92,51 @@ public class PlayerObj : NetworkBehaviour
 
     void DoMove()
     {
-        Vector3 _dirVec  = _goalPos - transform.position ;
-        Vector3 _disVec = (Vector2)_goalPos - (Vector2)transform.position ;
-        if( _disVec.sqrMagnitude < 0.1f )
+        Vector3 _dirVec = _goalPos - transform.position;
+        Vector3 _disVec = (Vector2)_goalPos - (Vector2)transform.position;
+        if (_disVec.sqrMagnitude < 0.1f)
         {
             _currentState = PlayerState.idle;
-            PlayStateAnimation(_currentState);
+            CMD_SetMove(_currentState);
             return;
         }
         Vector3 _dirMVec = _dirVec.normalized;
-        transform.position += (_dirMVec * _charMS * Time.deltaTime );
-        
-
-        if(_dirMVec.x > 0 ) _prefabs.transform.localScale = new Vector3(-1,1,1);
-        else if (_dirMVec.x < 0) _prefabs.transform.localScale = new Vector3(1,1,1);
+        transform.position += (_dirMVec * _charMS * Time.deltaTime);
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, playerManager.AvatarBoundary.bounds.min.x, playerManager.AvatarBoundary.bounds.max.x),
+            Mathf.Clamp(transform.position.y, playerManager.AvatarBoundary.bounds.min.y, playerManager.AvatarBoundary.bounds.max.y),
+            transform.position.z);
+        if (_dirMVec.x > 0) transform.localScale = new Vector3(-1.5f, 1.5f, 1.5f);
+        else if (_dirMVec.x < 0) transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
     }
-
+    [Client]
     public void SetMovePos(Vector2 pos)
     {
         _goalPos = pos;
         _currentState = PlayerState.run;
-        PlayStateAnimation(_currentState);
+        CMD_SetMove(_currentState);
+    }
+    [Command(requiresAuthority = true)]
+    public void CMD_SetMove(PlayerState state)
+    {
+        RPC_SetMove(state);
+    }
+    [ClientRpc]
+    public void RPC_SetMove(PlayerState state)
+    {
+        PlayStateAnimation(state);
+    }
+
+    [Client]
+    public void RemoveServerList()
+    {
+        playerManager.RemoveIdentityToList(myIdentity);
     }
     [Client]
     private void SendProfile()
     {
         Profile profile = ProfileManager.Instance.myProfile;
-        CMD_SendProfile(profile.name , profile.index , profile.imageMode , profile.defaultImage);
+        CMD_SendProfile(profile.name, profile.index, profile.imageMode, profile.defaultImage);
     }
 
     [Client]
@@ -121,7 +147,7 @@ public class PlayerObj : NetworkBehaviour
     }
 
     [Command]
-    private void CMD_SendProfile(string profilename , int profileIndex , bool profileMode , int profileImage)
+    private void CMD_SendProfile(string profilename, int profileIndex, bool profileMode, int profileImage)
     {
         int TempModeIndex;
         if (profileMode)
@@ -133,7 +159,7 @@ public class PlayerObj : NetworkBehaviour
 
             TempModeIndex = 0;
         }
-        ProfileManager.Instance.profileList.Add(new Profile(profilename , profileIndex , TempModeIndex, profileImage));
+        ProfileManager.Instance.profileList.Add(new Profile(profilename, profileIndex, TempModeIndex, profileImage));
         Debug.Log(ProfileManager.Instance.profileList.Count);
         for (int i = 0; i < ProfileManager.Instance.profileList.Count; i++)
         {
