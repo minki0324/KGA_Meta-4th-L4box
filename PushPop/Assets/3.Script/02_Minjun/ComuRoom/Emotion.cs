231f8704e227eac220ed8abfd5f3ab72e4db7867
Minjun <129065548+Nangdi@users.noreply.github.com>
2024-03-28 12:51:29 +0900

    networkScript Refactioring

--- PushPop/Assets/3.Script/02_Minjun/ComuRoom/Emotion.cs
+++ PushPop/Assets/3.Script/02_Minjun/ComuRoom/Emotion.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 using UnityEngine.UI;
 using Mirror;
 public class Emotion : NetworkBehaviour
-{
-    //단지 EMotion 들 저장하고 있기 위한 스크립트
-    public static Emotion instance;
-    //이모션 종류 인스펙터에서 추가하기
-    public Canvas canvas;
-    public Transform spawnPoint;
-    public PlayerEmotionControl playerEmotion;
-    private bool isExpend = false;
+{ //Emotion 관리와 실행 관련 스크립트
+
+    //public static Emotion instance;
+
+    //각로컬마다 각 PlayerEmotion을 player가 접속할때 start 에서 할당해줌
+    public PlayerEmotionControl playerEmotion; 
+    private bool isExpend = false; //감정표현 패널이 활성화 되있는지?
     [SerializeField] private GameObject emojiPanel;
 
+    //이모션 버튼에 넣어줄 Sprite
+    // *주의* PlayerEmotionControl 스크립트의 GameObject[] emojis;  와 순서 동일하게 둘것
     public Sprite[] sprites;
     public Button[] emojiBtns;
-    private void Awake()
-    {
-        instance = this;
-    }
+    //private void Awake()
+    //{
+    //    instance = this;
+    //}
     private void Start()
     {
         InitEmojiBtn();
@@ -31,9 +32,10 @@ public class Emotion : NetworkBehaviour
         //spriterenderer.sprite = emotions[index];
         playerEmotion.PlayEmotion(index);
     }
-
+    //감정표현 버튼에 onClick등록 되있음.
     public void ExpandEmojiPanel()
     {
+        //버튼누를때 이모지 패널 활성화 or 비활성화
         isExpend = !isExpend;
         emojiPanel.SetActive(isExpend);
     }
@@ -43,7 +45,9 @@ public class Emotion : NetworkBehaviour
         for (int i = 0; i < emojiBtns.Length; i++)
         {
             int index = i; // 클로저를 생성하여 인덱스를 복사합니다.
-            emojiBtns[i].onClick.AddListener(() => onEmotion(index)); //각버튼에 0~44 매개변수할당 
+            //이모지 실행 버튼에 onEmotion onClick메서드 할당
+            emojiBtns[i].onClick.AddListener(() => onEmotion(index));
+            //이모지sprites 배열 순서대로 이모지버튼에 sprite 넣어주기
             emojiBtns[i].GetComponent<Image>().sprite = sprites[i];
         }
     }
--- PushPop/Assets/3.Script/02_Minjun/ComuRoom/PlayerEmotionControl.cs
+++ PushPop/Assets/3.Script/02_Minjun/ComuRoom/PlayerEmotionControl.cs
@@ -11,9 +11,6 @@ public class PlayerEmotionControl : NetworkBehaviour
     private WaitForSeconds waitTime = new WaitForSeconds(2);
     [SerializeField] private GameObject[] emojis; 
     private Coroutine emojiCo;
-    private Vector3 myScale;
-    private Vector3 myPos;
-    float tempScaleX;
     //이모션 사진 직접참조
     [SerializeField] private GameObject currentEmoji;
     void Start()
@@ -21,17 +18,22 @@ public class PlayerEmotionControl : NetworkBehaviour
         TryGetComponent(out animator);
         if (isLocalPlayer)
         {
-            Emotion.instance.playerEmotion = this;
+            //자신의 씬에 있는 EmotionManager 에 자신의 이모션 스크립트 할당
+            FindObjectOfType<Emotion>().playerEmotion = this;
+            //Emotion.instance.playerEmotion = this;
         }
 
     }
     public void EMotionChange(int index)
     {
-        if(currentEmoji != null)
-        {
+        if(currentEmoji != null && currentEmoji.activeSelf)
+        {//현재 이모션이 실행중이라면?
+
+            //실행중인 코루틴 스탑, 켜져있는 이모지 false
             StopCoroutine(emojiCo);
             currentEmoji.SetActive(false);
         }
+        //실행할 이모지 변경
         currentEmoji = emojis[index];
       
        
@@ -50,16 +52,19 @@ public class PlayerEmotionControl : NetworkBehaviour
     public void RPC_Emotion(NetworkIdentity targetObject , int index)
     {
         //이모티콘 사용한 player 동기화 작업
-        //Emotion Play 한 Client EmotionPlayer 가져오기
+        //각 Client들마다 Emotion Play 한 Client EmotionPlayer 가져오기
         PlayerEmotionControl targetEmotionControl = targetObject.GetComponentInChildren<PlayerEmotionControl>();
         //선택한 이모션 Sprite로 교체
         targetEmotionControl.EMotionChange(index);
+        //이모지 실행
         emojiCo = StartCoroutine(EmojiPlay_co());
     }
     public IEnumerator EmojiPlay_co()
     {
+        //이모지실행
         currentEmoji.SetActive(true);
        yield return waitTime;
+        //이모지끄기
         currentEmoji.SetActive(false);
     }
 
